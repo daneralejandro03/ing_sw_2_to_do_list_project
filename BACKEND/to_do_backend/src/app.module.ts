0b7dfb68588cbecda4b9c48541ead1e77f3406d4
Daner Alejandro Salazar Colorado <daneralejandro03@gmail.com>
2025-03-10 09:31:32 -0500

    Environment Variables

--- BACKEND/to_do_backend/src/app.module.ts
+++ BACKEND/to_do_backend/src/app.module.ts
@@ -1,21 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456789',
-      database: 'task',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const portString = configService.get<string>('DB_PORT');
+        if (!portString) {
+          throw new Error('La variable de entorno DB_PORT no está definida.');
+        }
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(portString, 10),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
     }),
     TaskModule,
     CategoryModule,
